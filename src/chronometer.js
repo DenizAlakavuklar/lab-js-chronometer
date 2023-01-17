13b816b8d06006c4e8924bb4d5d22239f39ee941
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-01-17 14:17:09 +0100

    Completed all with only one red line on Jasmine

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval((printTimeCallback) => {
+    this.currentTime++;
+    }, 1000)
   }
+
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
+
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime) % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+       value = "0" + value; 
+    }
+    return `${value}`;
+  
   }
 
-  stop() {
-    // ... your code goes here
+stop() { 
+    return clearInterval (this.intervalId) ;
+   
   }
-
-  reset() {
-    // ... your code goes here
-  }
-
-  split() {
-    // ... your code goes here
-  }
+   
+reset() {
+  this.currentTime = 0;
 }
+  split() {
+
+   let minutes =  this.computeTwoDigitNumber(this.getMinutes());
+   let seconds =  this.computeTwoDigitNumber(this.getSeconds());
+  
+   return `${minutes}:${seconds}`;
+  
+  }
+} 
